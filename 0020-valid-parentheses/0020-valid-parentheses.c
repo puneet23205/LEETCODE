7e9f6c678aab546f82724707f342f7dbaa0d143a
puneet23205 <pandey.puneet0707@gmail.com>
2025-02-15 01:00:16 +0530

    Time: 0 ms (100%), Space: 7.9 MB (61.72%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -0,0 +1,27 @@
+#include <stdbool.h>
+#include <string.h>
+
+bool isValid(char* s) {
+    int len = strlen(s);
+    char stack[len];
+    int top = -1;
+
+    for (int i = 0; i < len; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            stack[++top] = s[i];
+        } else {
+            if (top == -1) {
+                return false;
+            }
+            if ((s[i] == ')' && stack[top] == '(') || 
+                (s[i] == '}' && stack[top] == '{') || 
+                (s[i] == ']' && stack[top] == '[')) {
+                top--;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return top == -1;
+}
