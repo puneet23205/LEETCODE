19b98bc324a07cdef029764e069f444010c22e62
puneet23205 <pandey.puneet0707@gmail.com>
2025-02-15 00:46:40 +0530

    Time: 2 ms (26.07%), Space: 9.4 MB (93.98%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.c
@@ -0,0 +1,19 @@
+int pivotIndex(int* nums, int numsSize) {
+    int totalSum = 0, leftSum = 0;
+
+    
+    for (int i = 0; i < numsSize; i++) {
+        totalSum += nums[i];
+    }
+
+    
+    for (int i = 0; i < numsSize; i++) {
+        int rightSum = totalSum - leftSum - nums[i];
+        if (leftSum == rightSum) {
+            return i; 
+        }
+        leftSum += nums[i]; 
+    }
+
+    return -1;
+}
