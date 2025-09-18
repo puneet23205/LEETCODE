04ed6b6c502acc0f7ce9cadddf675180af46437f
puneet23205 <pandey.puneet0707@gmail.com>
2025-09-18 19:33:27 +0530

    Time: 0 ms (100%), Space: 7.8 MB (62.62%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -0,0 +1,13 @@
+int climbStairs(int n) {
+    if(n == 1) return 1;
+    if(n == 2) return 2;
+
+    int prev1 = 2, prev2 = 1, curr;
+    for(int i = 3; i <= n; i++) {
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
+}
+ 
