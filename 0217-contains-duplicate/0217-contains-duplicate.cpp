d73edb484e4db052962e443c7bf1b20676e2991f
puneet23205 <pandey.puneet0707@gmail.com>
2025-10-03 16:57:40 +0530

    Time: 54 ms (40.56%), Space: 90.9 MB (31.33%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> visited;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (visited.find(nums[i]) != visited.end()) {
+                return true;
+            } else {
+                visited.insert(nums[i]);
+            }
+        }
+        return false;
+    }
+};
